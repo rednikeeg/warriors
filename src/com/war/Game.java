610eb0468d022bdc2a22b6ebd22863621ba459db
rednikeeg <maks.schoolforvv@gmail.com>
2021-09-21 22:43:12 +0300

    Game.fight() method completed

--- src/com/war/Game.java
+++ src/com/war/Game.java
@@ -1,7 +1,15 @@
 package com.war;
 
 public class Game {
+    private Game(){}
     public static boolean fight(Warrior w1, Warrior w2){
-        return true;
+        do{
+            w1.attack(w2);
+            if(w2.isAlive())
+                w2.attack(w1);
+        }
+        while(w1.isAlive() && w2.isAlive());
+
+        return w1.isAlive();
     }
 }
