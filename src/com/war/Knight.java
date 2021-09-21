21d52a96860e852d1744f6233e2d54bd993794d6
rednikeeg <maks.schoolforvv@gmail.com>
2021-09-21 22:42:12 +0300

    Warrior and Knight classes completed

--- src/com/war/Knight.java
+++ src/com/war/Knight.java
@@ -1,5 +1,8 @@
 package com.war;
 
 public class Knight extends Warrior{
-
+    public Knight(){
+        health = 50;
+        damage = 7;
+    }
 }
--- src/com/war/Warrior.java
+++ src/com/war/Warrior.java
@@ -1,5 +1,27 @@
 package com.war;
 
 public class Warrior {
+    protected int health;
+    protected int damage;
 
+    public Warrior(){
+        health = 50;
+        damage = 5;
+    }
+
+    public boolean isAlive(){
+        return health > 0;
+    }
+
+    public int getDamage(){
+        return damage;
+    }
+
+    public void attack(Warrior warrior){
+        warrior.takeDamage(damage);
+    }
+
+    public void takeDamage(int damage){
+        health -= damage;
+    }
 }
