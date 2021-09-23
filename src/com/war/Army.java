596b97e3498db63707f11db27e3c30ae3cb44f88
rednikeeg <maks.schoolforvv@gmail.com>
2021-09-23 23:19:03 +0300

    Army implementation

---
+++ src/com/war/Army.java
@@ -0,0 +1,35 @@
+package com.war;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Optional;
+
+public class Army {
+    private Deque<Warrior> army;
+
+    public Army() {
+
+    }
+
+    public Army(Class<? extends Warrior> cls, int quantity) throws ReflectiveOperationException {
+        army = new ArrayDeque<>();
+        var constr = cls.getConstructor();
+
+        for (int i = 0; i < quantity; i++) {
+            Warrior warrior = constr.newInstance();
+
+            army.add(warrior);
+        }
+    }
+
+    public Optional<Warrior> next() {
+        Optional<Warrior> result;
+
+        if (!army.isEmpty())
+            result = Optional.of(army.poll());
+        else
+            result = Optional.empty();
+
+        return result;
+    }
+}
--- src/com/war/Battle.java
+++ src/com/war/Battle.java
@@ -1,5 +1,7 @@
 package com.war;
 
+import java.util.Optional;
+
 public class Battle {
     private Battle() {
     }
@@ -13,5 +15,20 @@ public class Battle {
         while (w1.isAlive() && w2.isAlive());
 
         return w1.isAlive();
+    }
+
+    public static boolean fight(Army a1, Army a2) {
+        Optional<Warrior> w1 = a1.next();
+        Optional<Warrior> w2 = a2.next();
+        while (w1.isPresent() && w2.isPresent()) {
+            fight(w1.get(), w2.get());
+
+            if (!w1.get().isAlive())
+                w1 = a1.next();
+            else if (!w2.get().isAlive())
+                w2 = a2.next();
+        }
+
+        return w1.isPresent();
     }
 }
