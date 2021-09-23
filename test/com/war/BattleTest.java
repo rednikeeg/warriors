fb8a40dc922b262b0c0cc97f26775be44edb2037
rednikeeg <maks.schoolforvv@gmail.com>
2021-09-23 23:18:08 +0300

    Tests on Army made

--- test/com/war/BattleTest.java
+++ test/com/war/BattleTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BattleTest {
+    // Testing Warriors
     @Test
     @DisplayName("Warrior vs Knight assume false")
     void fight1() {
@@ -48,5 +49,51 @@ class BattleTest {
         boolean result = Battle.fight(hans, franz);
 
         assertTrue(result);
+    }
+
+    // Testing Armies
+
+    @Test
+    @DisplayName("1 Warrior vs 2 Warriors assume false")
+    void fight5() throws ReflectiveOperationException {
+        Army a1 = new Army(Warrior.class, 1);
+        Army a2 = new Army(Warrior.class, 2);
+
+        boolean result = Battle.fight(a1, a2);
+
+        assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("2 Warriors vs 2 Warriors assume true")
+    void fight6() throws ReflectiveOperationException {
+        Army a1 = new Army(Warrior.class, 2);
+        Army a2 = new Army(Warrior.class, 2);
+
+        boolean result = Battle.fight(a1, a2);
+
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("2 Knight vs 2 Warriors assume true")
+    void fight7() throws ReflectiveOperationException {
+        Army a1 = new Army(Knight.class, 2);
+        Army a2 = new Army(Warrior.class, 2);
+
+        boolean result = Battle.fight(a1, a2);
+
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("2 Warriors vs 2 Knights assume false")
+    void fight8() throws ReflectiveOperationException {
+        Army a1 = new Army(Warrior.class, 2);
+        Army a2 = new Army(Knight.class, 2);
+
+        boolean result = Battle.fight(a1, a2);
+
+        assertFalse(result);
     }
 }
